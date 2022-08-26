e584f435f95e6cdf1f6bd11d36c2e18d28b5ed83
andrebenedetti <andredbenedetti@gmail.com>
2022-08-26 10:12:08 -0300

    fix build

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -1,4 +1,4 @@
 import BigNumber from 'bignumber.js'
 
 export const toWei = (eth: string, decimals = 18) =>
-  new BigNumber(eth).times(`1e${decimals}`)
+  new BigNumber(eth).times(`1e${decimals}`).toString()
